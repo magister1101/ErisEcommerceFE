790abb05bea33dfb5368c70e7b8d8a38ac922534
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2025-04-14 04:23:50 +0800

    change config from history to hash

--- quasar.config.js
+++ quasar.config.js
@@ -41,7 +41,7 @@ export default defineConfig((/* ctx */) => {
         node: 'node20'
       },
 
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
